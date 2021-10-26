79f3e49bf2e399ab271ad56a4cef4e762a8ff979
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-10-25 20:42:51 -0700

    Devter

---
+++ bal/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React, {useState, useEffect} from 'react';
+
+function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => 
+  fetch('http://localhost:3000/users')
+  .then((res)=> res.json())
+  .then((json) => setUsers(json)),
+  []
+  );
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul> 
+          {users.map((user) => ( 
+            <li key={user.name}>{user.name}</li>
+          ))}
+        </ul>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
